543cb3886c7798358a2e4b357e1591492a032548
Donald Rauscher <donald.rauscher@gmail.com>
2016-11-28 22:45:50 -0600

    Add forced includes/excludes

--- optimize.R
+++ optimize.R
@@ -6,7 +6,8 @@ library(yaml)
 #	--obj-dir=min,max					...		character; whether objective is to minimize or maximize
 # --proj-percentile=0.7			...   numeric; percentile of output to optimize
 # --constraint='exp>=120'		...   character; constraint on either expectation or variance
-# --force-player='<NAME>'		...   character; force specific player into solution
+# --force-include='<NAME>'	...   character; force include specific player into solution
+# --force-exclude='<NAME>'	...   character; force exclude specific player into solution
 
 # get args
 args <- commandArgs(TRUE)
@@ -47,8 +48,9 @@ parse_constraint <- function(x){
 constraints <- args[which(names(args) == 'constraint')]
 constraints <- lapply(constraints, parse_constraint)
 
-# players to force into solution
-forced_players <- as.vector(args[which(names(args) == 'force-player')])
+# players to force into/out of solution
+forced_includes <- as.vector(args[which(names(args) == 'force-include')])
+forced_excludes <- as.vector(args[which(names(args) == 'force-exclude')])
 
 # inputs
 players	<- read.csv('./data/players.csv', header=TRUE, stringsAsFactors = FALSE)
@@ -69,8 +71,12 @@ for (c in constraints){
 	add.constraint(ip, xt = get(c$left), type = c$op, rhs = as.numeric(c$right))
 }
 
-for (p in forced_players){
+for (p in forced_includes){
 	add.constraint(ip, xt = ifelse(players$Player == p, 1, 0), type = "=", rhs = 1)
+}
+
+for (p in forced_excludes){
+	add.constraint(ip, xt = ifelse(players$Player == p, 1, 0), type = "=", rhs = 0)
 }
 
 add.constraint(ip, xt = players$Cost, type = "<=", rhs = meta$BUDGET)
